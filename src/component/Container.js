cb39981f988ae19fc2eaf2d528cbb852afcd18bf
md.ashekur <ashekur.rahman@eongroup.net.bd>
2018-06-24 10:41:53 +0600

    Refactor

--- src/component/Container.js
+++ src/component/Container.js
@@ -1,36 +1,36 @@
 import React, { Component } from "react";
 import PlayButton from "./PlayButton";
 export default class Container extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { isMusicPlaying: false };
-  }
-  handleClick() {
-    if (this.state.isMusicPlaying) {
-        window.audio.pause();
-    } else {
-      window.audio.play();
-    }
-    this.setState(prevState => {
-      return {
-        isMusicPlaying: !prevState.isMusicPlaying
-      };
-    });
-  }
-  render() {
-    return (
-      <div>         
-        <div id="container">
-          <div id="hook" />
-          <h1>Play Music</h1>
-          <input type="file" id="files" name="files[]" multiple />
-        </div>
-        <PlayButton
-          onClick={this.handleClick.bind(this)}
-          isMusicPlaying={this.state.isMusicPlaying}
-        />
-        <audio id="audio" />
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = { isMusicPlaying: false };
+	}
+	handleClick() {
+		if (this.state.isMusicPlaying) {
+			window.audio.pause();
+		} else {
+			window.audio.play();
+		}
+		this.setState(prevState => {
+			return {
+				isMusicPlaying: !prevState.isMusicPlaying
+			};
+		});
+	}
+	render() {
+		return (
+			<div>         
+				<div id="container">
+					<div id="hook" />
+					<h1>Play Music</h1>
+					<input type="file" id="files" name="files[]" multiple />
+				</div>
+				<PlayButton
+					onClick={this.handleClick.bind(this)}
+					isMusicPlaying={this.state.isMusicPlaying}
+				/>
+				<audio id="audio" />
+			</div>
+		);
+	}
 }
